9c6114bef759316711a2ca02f3a6ff3092e3c8e5
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-20 11:55:05 -0500

    se agrega el environment

--- src/app/service/consultar-cliente.service.ts
+++ src/app/service/consultar-cliente.service.ts
@@ -3,17 +3,18 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RequestDTO } from '../interfaces/requestDto';
 import { ResponseDTO } from '../interfaces/responseDto';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultarClienteService {
 
-  urlBase: string = 'http://localhost:8090/api/v1'
+  private URL_API = environment.urlBaseClient;
 
   constructor(private httpClient: HttpClient) { }
 
   consultarCliente(request: RequestDTO): Observable<ResponseDTO> {
-    return this.httpClient.post<ResponseDTO>(this.urlBase + "/cliente", request);
+    return this.httpClient.post<ResponseDTO>(this.URL_API + "/cliente", request);
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    urlBaseClient: 'http://localhost:8090/api/v1'
+};
